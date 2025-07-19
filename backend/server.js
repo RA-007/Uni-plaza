aa1a9a914e25b0c59f893d1d2bdba6af1dade713
ilangeeran <ilangoilangee1214@gmail.com>
2025-07-19 11:54:50 +0530

    basic

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+// Connect DB and start server
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .catch((err) => console.log(err));
